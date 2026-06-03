f3a51230afbc8656c81f8ab8ccd3e0896a747357
abrishcs <abrehamcheru15@gmail.com>
2026-06-03 10:45:05 +0300

    First commit

---
+++ lab1.py
@@ -0,0 +1,35 @@
+#print("hello world")
+
+#exe 1
+#name = input( "enter your name " )
+
+#print ("hello", name , "welcome!")
+
+#print(f"your name is {name }")
+#ex 3
+#age =20
+#if age >= 18:
+   # print("Eligible to vote")
+
+
+#ex 4
+
+#n =4 
+#for i in range(0,n):
+    # print(i)
+
+#ex class work 
+# for i in range(4):
+#     print((i-1)+"*"*i )
+for i in range(1, 5):
+    print(" " * (4 - i) + "*" * i)
+
+#ex 5
+# li= ["pyhton","for","trainee"]
+# for index in range(len(li)):
+#     print(li[index])
+
+
+
+
+
